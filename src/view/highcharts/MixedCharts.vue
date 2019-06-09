cc94956f21ae44855c2bd10c45cb396fff1bba4d
Iamwuxiangjie <992622784@qq.com>
2019-06-09 22:48:47 +0800

    2019-4-21 14:40

--- src/view/highcharts/MixedCharts.vue
+++ src/view/highcharts/MixedCharts.vue
@@ -1,19 +1,26 @@
 <template>
-  <div>
-    <div id="container">
-      <div id="detail"></div>
-      <div id="master" style="position:absolute;top:300px;height:100px;width:100%"></div>
-    </div>
-  </div>
+  <Row>
+    <Col span="24">
+      <Button @click="test">{{date}}</Button>
+    </Col>
+    <Col span="24">
+      <div id="container" style="hight:400px">
+        <div id="detail"></div>
+        <div id="master" style="position:absolute;top:300px;height:100px;width:100%"></div>
+      </div>
+    </Col>
+
+  </Row>
 
 </template>
 
 <script>
   import Highcharts from 'highcharts'
   import DateUtils from '../mixins/DateUtils.vue'
+  import ChartsUtils from '../mixins/ChartsUtils.vue'
 
   export default {
-    mixins:[DateUtils],
+    mixins:[DateUtils,ChartsUtils],
     name: "mixed-charts",
     data(){
       return {
@@ -42,7 +49,8 @@
               visible:true,
             }],
           detailCharts:{},
-        }
+        },
+        date:'123'
       }
     },
 
@@ -61,231 +69,14 @@
           past=this.add(past,1);
         }
       },
-
-      initCharts(){
-        let categories=this.dataList.map(i=>i.date);
-        const series=[];
-        const masterSeries=[];
-
-        const yAxis=[];
-        let fields=this.charts.fields;
-        fields.forEach((d, index) => {
-          series.push({
-            name: d.name,
-            type: d.type,
-            data: this.dataList.map(i => i[d.key]),
-            yAxis: index,
-            tooltip: {
-              valueSuffix: d.valueSuffix
-            },
-            visible:d.visible,
-          })
-
-          yAxis.push({ // Primary yAxis
-            tickWidth:0,//去掉刻度
-            gridLineWidth: 0,//去掉y轴方向的横线
-            labels: {
-              format: '{value}',
-              style: {
-                color: Highcharts.getOptions().colors[0]
-              },
-              enabled: false//去掉刻度数字
-            },
-            title: {
-              text: undefined,
-              style: {
-                color: Highcharts.getOptions().colors[1]
-              }
-            },
-            opposite: d.type === 'column'
-          })
-        })
-
-        masterSeries.push({
-          type: 'area',
-          name: fields[0].name,
-          pointInterval: 24 * 3600 * 1000,
-          pointStart: this.dateParse(categories[0]).getTime(),
-          data: this.dataList.map(i => i[fields[0].key])
-        })
-
-        const from=this.dateParse(categories[0]).getTime();
-        const to=this.dateParse(categories[categories.length-1]).getTime();
-        this.$nextTick(() => {
-            let masterData=Array.from(this.dataList);
-            let detailData=[]
-
-            const master=this.$el.querySelector("#master");
-            const detail=this.$el.querySelector("#detail");
-
-            let masterCharts;
-            let detailCharts;
-
-            const initDetailCharts=()=>{
-              detailCharts = Highcharts.chart(detail, {
-              chart: {
-                zoomType: 'xy',
-                marginBottom: 120,
-                reflow: false,
-                marginLeft: 50,
-                marginRight: 20,
-                style: {
-                  position: 'absolute'
-                }
-              },
-              credits: {
-                enabled: false
-              },
-              title: {
-                text: '价格分析',
-                align:'left'
-              },
-              xAxis: [{
-                categories: categories,
-                crosshair: true
-              }],
-              yAxis: yAxis,
-              tooltip: {
-                shared: true
-              },
-              legend: {
-                layout: 'horizontal',
-                align: 'right',
-                verticalAlign: 'top',
-                floating: true,
-                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-              },
-              series: series
-            });
-            }
-            const initMasterCharts=()=>{
-              masterCharts=Highcharts.chart(master, {
-                chart: {
-                  reflow: false,
-                  borderWidth: 0,
-                  backgroundColor: null,
-                  marginLeft: 50,
-                  marginRight: 20,
-                  zoomType: 'x',
-                  events: {
-                    selection: function (event) {
-                      let extremesObject = event.xAxis[0];
-                      let min = extremesObject.min;
-                      let max = extremesObject.max;
-                      detailData = [];
-                      let xAxis = this.xAxis[0];
-                      // move the plot bands to reflect the new detail span
-                      xAxis.removePlotBand('mask-before');
-                      xAxis.addPlotBand({
-                        id: 'mask-before',
-                        from: from,
-                        to: min,
-                        color: 'rgba(0, 0, 0, 0.2)'
-                      });
-                      xAxis.removePlotBand('mask-after');
-                      xAxis.addPlotBand({
-                        id: 'mask-after',
-                        from: max,
-                        to: to,
-                        color: 'rgba(0, 0, 0, 0.2)'
-                      });
-                      detailData=masterData.filter(i=>{
-                        const time=new Date(i.date.replace(/-/g, '/')).getTime();
-                        const result=time>=min&&time<=max;
-                        return result;
-                      })
-                      detailCharts.xAxis[0].setCategories(detailData.map(i=>i.date));
-                      fields.forEach((d, index) => {
-                        detailCharts.series[index].setData(detailData.map(i => i[d.key]));
-                      })
-                      return false;
-                    }
-                  }
-                },
-                title: {
-                  text: null
-                },
-                xAxis: {
-                  type: 'datetime',
-                  dateTimeLabelFormats: {
-                    day: '%y-%m-%d',
-                    week: '%y-%m-%d',
-                    month: '%Y-%m',
-                    year: '%Y'
-                  },
-                  showLastTickLabel: true,
-                  maxZoom: 2 * 24 * 3600000, // fourteen days
-                  plotBands: [{
-                    id: 'mask-before',
-                    from: from,
-                    to: to,
-                    color: 'rgba(0, 0, 0, 0.2)'
-                  }],
-                  title: {
-                    text: null
-                  }
-                },
-                yAxis: {
-                  gridLineWidth: 0,
-                  labels: {
-                    enabled: false
-                  },
-                  title: {
-                    text: null
-                  },
-                  min: 0.6,
-                  showFirstLabel: false
-                },
-                tooltip: {
-                  formatter: function () {
-                    return false;
-                  }
-                },
-                legend: {
-                  enabled: false
-                },
-                credits: {
-                  enabled: false
-                },
-                plotOptions: {
-                  series: {
-                    fillColor: {
-                      linearGradient: [0, 0, 0, 70],
-                      stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, 'rgba(255,255,255,0)']
-                      ]
-                    },
-                    lineWidth: 1,
-                    marker: {
-                      enabled: false
-                    },
-                    shadow: false,
-                    states: {
-                      hover: {
-                        lineWidth: 1
-                      }
-                    },
-                    enableMouseTracking: false
-                  }
-                },
-                series: masterSeries,
-                exporting: {
-                  enabled: false
-                }
-              }, function () {
-                initDetailCharts();
-              });
-            }
-            initMasterCharts();
-        });
-      },
-
+      test(){
+        this.date=Highcharts.dateFormat('%Y-%m-%d %H:%M:%S',new Date().getTime(),false);
+      }
     },
 
     mounted(){
         this.init();
-        this.initCharts();
+        this.initCharts(this.dataList,this.charts.fields);
     },
   }
 </script>
